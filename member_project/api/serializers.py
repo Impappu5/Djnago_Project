1aca226cbbec1fd67eac5c6799334f51f41ca74b
pappu1234590676 <102293598+pappu1234590676@users.noreply.github.com>
2026-01-31 16:15:55 +0530

    Add Toast Popup

--- member_project/api/serializers.py
+++ member_project/api/serializers.py
@@ -20,17 +20,17 @@ class MemberSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
-    class Meta:
-        model = User
-        fields = ["id", "username", "email", "password"]
+    # class Meta:
+    #     model = User
+    #     fields = ["id", "username", "email", "password"]
 
-    def create(self, validated_data):
-        user = User.objects.create_user(
-            email=validated_data["email"],
-            username=validated_data["username"],
-            password=validated_data["password"],
-        )
-        return user
+    # def create(self, validated_data):
+    #     user = User.objects.create_user(
+    #         email=validated_data["email"],
+    #         username=validated_data["username"],
+    #         password=validated_data["password"],
+    #     )
+    #     return user
 
     password = serializers.CharField(write_only=True)
 
